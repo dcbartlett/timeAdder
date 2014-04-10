b619db855d45ba70017cfd838daa99bdc812e13b
Dennis Bartlett <bartlett.dc.1@gmail.com>
2014-04-10 11:52:29 -0500

    Added timeAdd.js API function

--- api/timeAdd.js
+++ api/timeAdd.js
@@ -1,32 +1,26 @@
-module.exports = function (start, add) {
+module.exports = function addTime(startTime, addMinutes) {
 
 	// [H]H:MM {AM|PM}
 	// 9:23 AM + 10 = 9:33 AM
 	
-	var minutes = getTimeInMinutes(start);
+	var minutes = getTimeInMinutes(startTime);
+	minutes = parseInt(minutes) + parseInt(addMinutes);
 	var timeArray = setTimeInMinutes(minutes);
 
 	time = "" + timeArray[0][0] + ":" + timeArray[0][1] + " " + timeArray[1];
-	console.log(time);
-
 	return time;
-}
-
-var minutes = getTimeInMinutes(process.argv[2]) + 10;
-setTimeInMinutes(minutes, "12");
-
+};
 
 function getTimeInMinutes(timeString) {
 	var timeArray = timeString.split(" ");
 	timeArray[0] = timeArray[0].split(":");
 
-	console.log(timeArray)
 	if (timeArray[1] && timeArray[1] != "AM" && timeArray[1] != "PM") {
-		return {error: {msg: "Invalid Period of Time: Suggestions AM/PM"}}
+		return {error: {msg: "Invalid Period of Time: Suggestions AM/PM"}};
 	}
 
 	if (timeArray[1] == "PM") {
-		if (!(timeArray[0][0] == 12)) {
+		if (timeArray[0][0] != 12) {
 			timeArray[0][0] = parseInt(timeArray[0][0]) + 12;
 		}
 	} else {
@@ -42,18 +36,22 @@ function setTimeInMinutes(minutes) {
 	var timeArray = [];
 	timeArray[0] = [];
 	timeArray[0][0] = parseInt(minutes/60);
-	timeArray[0][1] = parseInt(minutes%60);
+	timeArray[0][1] = parseInt(minutes%60) < 10? "0" + parseInt(minutes%60) : parseInt(minutes%60);
 	timeArray[1] = parseInt(minutes/60) > 11? "PM" : "AM";
 	
+	if (minutes > 1439 ) {
+		minutes -= 1440;
+		timeArray[1] = "AM";
+	}
+
 	if (timeArray[0][0] > 12) {
 		timeArray[0][0] = timeArray[0][0] - 12;
 	}
 	if (timeArray[1] == "AM") {
-		if (timeArray[0][0] == 0) {
+		if (timeArray[0][0] === 0) {
 			timeArray[0][0] = 12;
 		}
 	}
 	
-	console.log(timeArray)
 	return timeArray;
 }
